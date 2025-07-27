62baa3e6e7c8d049aa1d899c0c3ce11c2a4b5264
Shikhar Singh <146508977+spywar18@users.noreply.github.com>
2025-07-27 23:34:59 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -13,7 +13,6 @@ import Footer from './components/Footer'
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import Verify from './pages/Verify'
-
 const App = () => {
   return (
     <div className='mx-4 sm:mx-[10%]'>
